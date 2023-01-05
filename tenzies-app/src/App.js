e4dc8ad44d83e6855659f5e7f6b4c4cc267ecf3b
danyatcode <danyatd22@gmail.com>
2023-01-05 23:44:59 +0200

    Update

--- tenzies-app/src/App.js
+++ tenzies-app/src/App.js
@@ -6,19 +6,40 @@ function App() {
 
   const [dice, setDice] = React.useState(allNewDice());
 
-    const [tenzies, setTenzies] = React.useState(false)
+    const [tenzies, setTenzies] = React.useState(false);
     
+    const [numRolls, setNumRolls] = React.useState(0);
 
+    const [time, setTime] = React.useState()
+
+    const [record, setRecord] = React.useState(JSON.parse(localStorage.getItem("record")) || [])
+
+    const [bestRecord, setBestRecord] = React.useState()
+    
     React.useEffect(() => {
       if(dice.every(tenzy => dice[0].value === tenzy.value && tenzy.isHeld)){
         setTenzies(true);
-        console.log('won')
+        setTime(prevTime => Math.round((new Date() - prevTime) / 1000));
+        localStorage.setItem("record", JSON.stringify(record))
+        setRecord(prevRecord => [...prevRecord, numRolls])
+        setBestRecord((record.sort( (a, b) => b - a))[record.length-1])
       }
       else{
         setTenzies(false)
       }
     }, [dice])
 
+  function checkTime(getTime){
+    if (getTime > 59 && getTime < 3600){
+      return <div>You won this with {getTime/60} minutes!🎉</div> 
+    }
+    if(getTime > 3600){
+      return <div style={{display : 'none'}}></div>
+    }
+    else{
+      return <div>You won this with {getTime} seconds!🎉</div> 
+    }
+  }
 
   function generateNewDie(){
     return {
@@ -48,26 +69,30 @@ function App() {
   }
 
   function rollBtn(){
+    if(numRolls === 0){
+      setTime(new Date())
+    }
+    setNumRolls(prevNum => prevNum + 1);
     if(!tenzies){setDice(prevDice => prevDice.map(elem => {
       {if(elem.isHeld){
         return elem
       }else{
-        return generateNewDie()
+        return generateNewDie();
       }
     }}))
   }
-  else{
-    setDice(allNewDice())
-  }
+    else{
+      setDice(allNewDice())
+      setNumRolls(0);
+    }
 }
-
   const diceElements = dice.map(element => <Die 
     key={element.id} 
     value={element.value} 
     isHeld={element.isHeld} 
     holdDice={() => {holdDice(element.id)}}
   />)
-
+  
   return (
       <main>
           <h1 className="title">Tenzies</h1>
@@ -79,6 +104,10 @@ function App() {
           </div>
           
           <button onClick={rollBtn} className="roll-btn">{tenzies?"Start new game":"Roll"}</button>
+
+          <h2>{tenzies? <span>You won with count of rolls: {numRolls}</span>: <span>Count of your rolls: {numRolls} ✨</span>}</h2>
+          <h2>{tenzies?<span>{checkTime(time)}</span>: false}</h2>
+          {tenzies && record.length > 1 && <strong>Your previous best record is: {bestRecord}! 🔥</strong>}
           {tenzies && <Congrats />}
       </main>
   );
--- tenzies-app/src/Die.js
+++ tenzies-app/src/Die.js
@@ -5,7 +5,51 @@ export default function Die(props){
                 className="tenzy" 
                 onClick={props.holdDice}
             >
-                {props.value} 
+                {props.value === 6 && 
+                <div style={{flexWrap : "wrap", display: 'flex', width: '100%', height: '100%', alignItems : 'center', justifyContent : 'space-evenly'}}>
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                </div>
+                } 
+                {props.value === 5 && 
+                <div style={{flexWrap : "wrap", display: 'flex', width: '100%', height: '100%', alignItems : 'center', justifyContent : 'space-evenly'}}>
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                    <div className="dots" style={{width : '100%'}}></div>    
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                </div>
+                } 
+                {props.value === 4 && 
+                <div style={{flexWrap : "wrap", display: 'flex', width: '100%', height: '100%', alignItems : 'center', justifyContent : 'space-evenly'}}>
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                    <div className="dots"></div>     
+                </div>
+                } 
+                {props.value === 3 && 
+                <div style={{flexWrap : "wrap", display: 'flex', width: '100%', height: '100%', alignItems : 'center', justifyContent : 'space-evenly'}}>
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                    <div className="dots"></div>     
+                </div>
+                } 
+                {props.value === 2 && 
+                <div style={{flexWrap : "wrap", display: 'flex', width: '100%', height: '100%', alignItems : 'center', justifyContent : 'space-evenly'}}>
+                    <div className="dots"></div>    
+                    <div className="dots"></div>    
+                </div>
+                } 
+                {props.value === 1 && 
+                <div style={{flexWrap : "wrap", display: 'flex', width: '100%', height: '100%', alignItems : 'center', justifyContent : 'space-evenly'}}>
+                    <div className="dots"></div>    
+                </div>
+                } 
             </div>
     )
 }
